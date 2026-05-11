7f81da408c00d4365be817adedd1c33cb664d81c
Silvertakana <titmitna@gmail.com>
2026-05-12 01:48:18 +1200

    fix: add tsup config to inline @wwv-seeders/shared in build output

---
+++ packages/wildfires/tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts'],
+  format: ['esm'],
+  clean: true,
+  noExternal: [/@wwv-seeders\/.*/],
+  external: [/^(?!@wwv-seeders)[a-z@].*/],
+});
